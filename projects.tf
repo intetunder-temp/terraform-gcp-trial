1a4f613d87a476e9be3e955505746ef201c8480a
intetunder-temp <github-temp@darknet.fyi>
2020-04-21 18:33:28 +0100

    forgot terraform fmt, vim should do it, but not doing it for some reason

--- projects.tf
+++ projects.tf
@@ -1,10 +1,10 @@
 module gke_project {
   source              = "git::https://github.com/intetunder-temp/terraform-modules/project"
-  project_name        = "GKE Project" 
+  project_name        = "GKE Project"
   project_id          = "sander-gke-project"
   owner_access        = "user:sander-fearnley-hvas@darknet.fyi"
   auto_create_network = true
-  activate_apis       = [ "compute.googleapis.com", "iam.googleapis.com", "iamcredentials.googleapis.com", "storage-api.googleapis.com", "storage-component.googleapis.com" ]
+  activate_apis       = ["compute.googleapis.com", "iam.googleapis.com", "iamcredentials.googleapis.com", "storage-api.googleapis.com", "storage-component.googleapis.com"]
   labels = {
     team        = "sander"
     environment = "production"
