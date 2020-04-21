57a624166194525e8968dce224f11cbc93357c2f
intetunder-temp <github-temp@darknet.fyi>
2020-04-21 19:57:17 +0100

    adding networking to the gke project

--- projects.tf
+++ projects.tf
@@ -3,7 +3,7 @@ module gke_project {
   project_name        = "GKE Project"
   project_id          = "sander-gke-project"
   owner_access        = "user:sander-fearnley-hvas@darknet.fyi"
-  auto_create_network = true
+  auto_create_network = false
   billing_account     = "0191B0-82ED5F-1ABE8F"
   org_id              = ""
   activate_apis       = ["compute.googleapis.com", "iam.googleapis.com", "iamcredentials.googleapis.com", "storage-api.googleapis.com", "storage-component.googleapis.com", "container.googleapis.com"]
