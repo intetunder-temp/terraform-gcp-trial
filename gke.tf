2c5ea28a1284341285c8f88c959ce540ed9f1dc6
intetunder-temp <github-temp@darknet.fyi>
2020-04-22 02:02:35 +0100

    updating node_pool

--- gke.tf
+++ gke.tf
@@ -1,13 +1,13 @@
-module gke_web_stg_container_cluster {
+module gke_dev_container_cluster {
   source       = "git::https://github.com/intetunder-temp/terraform-modules.git//container_cluster"
   gke_project  = module.gke_project.project_id
   kube_version = "1.14"
 
-  network                    = module.network_gke.network_self_link
-  gke_subnetwork             = module.subnetwork_gke.subnetwork_self_link[0]
-  distribution_policy_zones  = ["europe-west4-a", "europe-west4-b", "europe-west4-c"]
+  network        = module.network_gke.network_self_link
+  gke_subnetwork = module.subnetwork_gke.subnetwork_self_link[0]
+  #distribution_policy_zones  = ["europe-west4-a", "europe-west4-b", "europe-west4-c"]
   cluster_name               = "gke"
-  region                     = "europe-west4"
+  region                     = "europe-west4-a"
   cluster_label              = "gke"
   workload_label             = "gke"
   team_label                 = "gke"
@@ -22,23 +22,23 @@ module gke_web_stg_container_cluster {
   master_ipv4_cidr_block     = "172.16.0.16/28"
 }
 
-module gke_web_stg_web {
+module gke_dev_default {
   source          = "git::https://github.com/intetunder-temp/terraform-modules.git//node_pool"
   cluster_name    = "gke"
   name            = "default"
-  autoscaling     = false
+  autoscaling     = true
   node_count      = "1"
-  min_node_count  = "0"
-  max_node_count  = "0"
+  min_node_count  = "1"
+  max_node_count  = "3"
   max_pods        = "110"
   local_ssd_count = "0"
-  region          = "europe-west4"
+  region          = "europe-west4-a"
   auto_repair     = "true"
   auto_upgrade    = "true"
   image_type      = "COS"
   disk_size_gb    = "30"
   disk_type       = "pd-ssd"
-  machine_type    = "n1-standard-2"
+  machine_type    = "custom-2-13312"
   n2_instance     = false
   preemptible     = "false"
   node_metadata   = "SECURE"
@@ -60,6 +60,38 @@ module gke_web_stg_web {
         effect = "NO_SCHEDULE"
       }
     ]
+  }
+}
+
+module gke_dev_system {
+  source          = "git::https://github.com/intetunder-temp/terraform-modules.git//node_pool"
+  cluster_name    = "gke"
+  name            = "system"
+  autoscaling     = false
+  node_count      = "1"
+  min_node_count  = "0"
+  max_node_count  = "0"
+  max_pods        = "110"
+  local_ssd_count = "0"
+  region          = "europe-west4-a"
+  auto_repair     = "true"
+  auto_upgrade    = "true"
+  image_type      = "COS"
+  disk_size_gb    = "30"
+  disk_type       = "pd-ssd"
+  machine_type    = "n1-standard-2"
+  n2_instance     = false
+  preemptible     = "false"
+  node_metadata   = "SECURE"
+  gke_project     = module.gke_project.project_id
+
+  node_pools_labels = {
+    all    = {}
+    system = {}
+  }
+  node_pools_taints = {
+    all    = []
+    system = []
   }
 }
 
