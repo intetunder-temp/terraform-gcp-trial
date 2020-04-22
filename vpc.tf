a3d673d69783b72b8e3cd8088e57efb55bdaebf1
intetunder-temp <github-temp@darknet.fyi>
2020-04-23 00:10:10 +0100

    adding vpc

--- vpc.tf
+++ vpc.tf
@@ -38,4 +38,45 @@ module "cloudnat-gke-netherlands" {
   project = module.network_gke.network_project
   network = module.network_gke.network_name
 }
+#### STG ####
+module network_stg_gke {
+  source          = "git::https://github.com/intetunder-temp/terraform-modules.git//network"
+  network_name    = "network-gke"
+  network_project = module.gke_stg_project.project_id
+  description     = "GKE STG VPC"
+  routing_mode    = "GLOBAL"
+}
+module subnetwork_stg_gke {
+  source                   = "git::https://github.com/intetunder-temp/terraform-modules.git//subnetwork"
+  name                     = "sn-gke"
+  project                  = module.network_stg_gke.network_project
+  description              = "GKE STG Subnet"
+  subnet_cidr              = "10.2.64.0/18"
+  region                   = "europe-west6"
+  network                  = module.network_stg_gke.network_self_link
+  private_ip_google_access = true
+  flow_logs                = "false"
+  secondary_ip_range = {
+    sn-gke = [
+      {
+        ip_cidr_range = "10.2.128.0/17"
+        range_name    = "pods"
+      },
+      {
+        ip_cidr_range = "10.2.0.0/19"
+        range_name    = "services"
+      },
+      {
+        ip_cidr_range = "10.2.32.0/19"
+        range_name    = "misc"
+      }
+    ]
+  }
+}
+module "cloudnat-gke-zurich" {
+  source  = "git::https://github.com/intetunder-temp/terraform-modules.git//cloudnat"
+  region  = "europe-west6"
+  project = module.network_stg_gke.network_project
+  network = module.network_stg_gke.network_name
+}
 
