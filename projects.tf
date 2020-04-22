ab2fda4cd72398ee0c819bbecb4f92d0971976b9
intetunder-temp <github-temp@darknet.fyi>
2020-04-22 16:17:48 +0100

    elastic operator

--- projects.tf
+++ projects.tf
@@ -9,7 +9,24 @@ module gke_project {
   activate_apis       = ["compute.googleapis.com", "iam.googleapis.com", "iamcredentials.googleapis.com", "storage-api.googleapis.com", "storage-component.googleapis.com", "container.googleapis.com"]
   labels = {
     team        = "sander"
-    environment = "production"
+    environment = "dev"
     purpose     = "gke"
   }
 }
+
+module gke_stg_project {
+  source              = "git::https://github.com/intetunder-temp/terraform-modules.git//project"
+  project_name        = "GKE STG Project"
+  project_id          = "focus-ensign-274916"
+  owner_access        = "user:sander-fearnley-hvas@darknet.fyi"
+  auto_create_network = false
+  billing_account     = "0191B0-82ED5F-1ABE8F"
+  org_id              = ""
+  activate_apis       = ["compute.googleapis.com", "iam.googleapis.com", "iamcredentials.googleapis.com", "storage-api.googleapis.com", "storage-component.googleapis.com", "container.googleapis.com"]
+  labels = {
+    team        = "sander"
+    environment = "stg"
+    purpose     = "gke"
+  }
+}
+
