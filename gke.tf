fb96820e9aa950c904ac19324cb6b336c9d3b390
intetunder-temp <github-temp@darknet.fyi>
2020-04-22 15:24:10 +0100

    updating k8s

--- gke.tf
+++ gke.tf
@@ -1,7 +1,7 @@
 module gke_dev_container_cluster {
   source       = "git::https://github.com/intetunder-temp/terraform-modules.git//container_cluster"
   gke_project  = module.gke_project.project_id
-  kube_version = "1.14"
+  kube_version = "1.15"
 
   network        = module.network_gke.network_self_link
   gke_subnetwork = module.subnetwork_gke.subnetwork_self_link[0]
@@ -29,7 +29,7 @@ module gke_dev_default {
   autoscaling     = true
   node_count      = "1"
   min_node_count  = "1"
-  max_node_count  = "3"
+  max_node_count  = "2"
   max_pods        = "110"
   local_ssd_count = "0"
   region          = "europe-west4-a"
@@ -48,7 +48,6 @@ module gke_dev_default {
     all = {}
     default = {
       workload = "default"
-      suffix   = "01"
     }
   }
   node_pools_taints = {
@@ -67,10 +66,10 @@ module gke_dev_system {
   source          = "git::https://github.com/intetunder-temp/terraform-modules.git//node_pool"
   cluster_name    = "gke"
   name            = "system"
-  autoscaling     = false
+  autoscaling     = true
   node_count      = "1"
-  min_node_count  = "0"
-  max_node_count  = "0"
+  min_node_count  = "1"
+  max_node_count  = "2"
   max_pods        = "110"
   local_ssd_count = "0"
   region          = "europe-west4-a"
@@ -79,7 +78,7 @@ module gke_dev_system {
   image_type      = "COS"
   disk_size_gb    = "30"
   disk_type       = "pd-ssd"
-  machine_type    = "n1-standard-2"
+  machine_type    = "custom-1-13312-ext"
   n2_instance     = false
   preemptible     = "false"
   node_metadata   = "SECURE"
